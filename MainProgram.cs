fda9cd757025a9b13f8c772b78d3cfcf1688f2f8
Ahkam Nihardeen <ahkamyaqeen@outlook.com>
2019-06-19 23:56:42 +1000

    Added .NET core
    Added .NET core and .gitignore for .NED core

---
+++ MainProgram.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DyTrailer
+{
+    class MainProgram
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
